62348946c9239ed858a53c9cf50edeb683d44c87
Geovane Maia <geovanemaia.contato@gmail.com>
2021-03-25 11:36:34 -0300

    Arrays, Vetores

---
+++ cursojavabasico/Vetores.java
@@ -0,0 +1,44 @@
+package cursojavabasico;
+
+import java.util.ArrayList;
+
+/*
+ * Vetores, Arrays
+ * 
+ * versão 1.0
+ * 
+ * autor: Geovane Maia
+ */
+public class Vetores {
+
+	public static void main(String[] args) {
+		
+		int numeros[] = new int[5];
+		
+		numeros[0]= 10;
+		numeros[1]= 20;
+		numeros[2]= 30;
+		numeros[3]= 40;
+		numeros[4]= 50;
+		
+		System.out.println(numeros[2]);
+		
+		char consoantes[] = {'b','c','d','e','f','g'};
+		System.out.println(consoantes[4]);
+		
+		ArrayList<Integer> pares = new ArrayList<>();
+		
+		pares.add(2);
+		pares.add(6);
+		pares.add(1, 4);
+		pares.add(24);
+		pares.add(10);
+		pares.add(8);
+		pares.sort(null);
+		pares.remove(4);
+		
+		System.out.println(pares.get(4));
+
+	}
+
+}
