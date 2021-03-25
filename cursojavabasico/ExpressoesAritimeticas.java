634a96655d5fe10d8e1558126e32c2dee21b4dcc
Geovane Maia <geovanemaia.contato@gmail.com>
2021-03-25 11:17:42 -0300

    Classe Math

---
+++ cursojavabasico/ExpressoesAritimeticas.java
@@ -0,0 +1,25 @@
+package cursojavabasico;
+/*
+ *Classe de explicação 
+ *
+ * versão 1.0
+ * 
+ * autor: Geovane Maia
+ */
+public class ExpressoesAritimeticas {
+	
+	public static void main(String[] args) {
+		
+		//int resultado = (10 + 2) * 3 / 2;
+		
+		//boolean resultado = 27 + 40 > 100 - 50;
+		
+		//resultado = 30 == 3 * 10 && 50 < 15;
+		
+		int resultado = (int) Math.pow(2, 10);
+		
+		System.out.println(Math.abs(-64));
+		
+		System.out.println(resultado);
+	}
+}
